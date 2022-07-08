47fe5ba5f54a4249c089fffb588d971f3ac9f464
imonem <amr.nabeel@gmail.com>
2022-07-09 00:58:01 +0200

    REST API Done/ MongoDB CRUD Operations working

--- backend/controllers/issuesController.js
+++ backend/controllers/issuesController.js
@@ -1,8 +1,13 @@
 const asyncHandler = require('express-async-handler');
+const { update } = require('../models/issueModel');
+
+const Issue = require('../models/issueModel');
 
 //Get tickets, route /api/issues, access Private
 const getIssues = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'There are your tickets!!' });
+	const issues = await Issue.find();
+
+	res.status(200).json(issues);
 });
 
 //Get tickets, route /api/issues, access Private
@@ -11,16 +16,39 @@ const createIssues = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error('Please add a text field');
 	}
-	res.status(200).json({ message: 'Ticket created!!' });
+	const issue = await Issue.create({
+		text: req.body.text,
+	});
+	res.status(200).json(issue);
 });
 
 //Get tickets, route /api/issues/:id, access Private
 const updateIssue = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Ticket ${req.params.id} updated!!` });
+	const issue = await Issue.findById(req.params.id);
+
+	if (!issue) {
+		res.status(400);
+		throw new Error('Issue not found');
+	}
+
+	const updatedIssue = await Issue.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
+
+	res.status(200).json(updatedIssue);
 });
 
 //Get tickets, route /api/issues/:id, access Private
 const deleteIssue = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Ticket ${req.params.id} deleted!!` });
+	const issue = await Issue.findById(req.params.id);
+
+	if (!issue) {
+		res.status(400);
+		throw new Error('Issue not found');
+	}
+
+	await Issue.deleteOne();
+
+	res.status(200).json({ id: req.params.id });
 });
 module.exports = { getIssues, createIssues, updateIssue, deleteIssue };
