c21511f80876d4460a5b0030ab30397e0b1f26ce
imonem <amr.nabeel@gmail.com>
2022-07-09 00:05:53 +0200

    MongoDB connection successfully established

--- backend/controllers/issuesController.js
+++ backend/controllers/issuesController.js
@@ -1,19 +1,26 @@
+const asyncHandler = require('express-async-handler');
+
 //Get tickets, route /api/issues, access Private
-const getIssues = (req, res) => {
+const getIssues = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: 'There are your tickets!!' });
-};
+});
 
 //Get tickets, route /api/issues, access Private
-const createIssues = (req, res) => {
+const createIssues = asyncHandler(async (req, res) => {
+	if (!req.body.text) {
+		res.status(400);
+		throw new Error('Please add a text field');
+	}
 	res.status(200).json({ message: 'Ticket created!!' });
-};
+});
 
 //Get tickets, route /api/issues/:id, access Private
-const updateIssue = (req, res) =>
+const updateIssue = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `Ticket ${req.params.id} updated!!` });
+});
 
 //Get tickets, route /api/issues/:id, access Private
-const deleteIssue = (req, res) =>
+const deleteIssue = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `Ticket ${req.params.id} deleted!!` });
-
+});
 module.exports = { getIssues, createIssues, updateIssue, deleteIssue };
